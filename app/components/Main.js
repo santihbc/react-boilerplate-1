fe9281817bf1e6aad302d19bcaa3894bedcc1943
Joacim Löwgren <joacim.lowgren@gmail.com>
2016-01-16 14:22:31 +0100

    Add a bit o' styling ✨

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
   render() {
     return (
       <div role="main">
-        <p>Content</p>
+        <p>Macaroon caramels carrot cake gingerbread sweet marshmallow. Biscuit apple pie brownie brownie gummi bears wafer gummies. Cake bear claw marzipan carrot cake croissant halvah cookie cheesecake. Bonbon cupcake ice cream cupcake soufflé cheesecake soufflé soufflé candy canes. Chocolate bar tiramisu jelly beans lemon drops. Candy canes bonbon ice cream marshmallow marshmallow donut. Gummi bears sesame snaps toffee sweet bonbon dragée fruitcake icing. Tiramisu cupcake sesame snaps cheesecake. Chocolate bar gummies gummi bears. Lemon drops dessert sweet roll cheesecake sesame snaps dessert.</p>
       </div>
     );
   }
